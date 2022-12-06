c03d3065e68c6373583d1e77e4e189cfd674e0e3
kwplummer <kwplummer@gmail.com>
2022-12-05 21:30:18 -0800

    Formatting.

--- 2022/kotlin-2022/src/main/java/zone/frog/advent/twentytwo/Day6.kt
+++ 2022/kotlin-2022/src/main/java/zone/frog/advent/twentytwo/Day6.kt
@@ -5,10 +5,11 @@ import java.io.File
 object Day6 {
     private fun allUnique(chars: String) = chars.toSet().size == chars.length
 
-    private fun firstUnique(textFile: String, groupSize: Int) = File(textFile).readText()
-        .windowed(groupSize, 1).withIndex()
-        .firstNotNullOf { indexedChars -> indexedChars.index.takeIf { allUnique(indexedChars.value) } }
-        .let { it + groupSize }
+    private fun firstUnique(textFile: String, groupSize: Int) =
+        File(textFile).readText()
+            .windowed(groupSize, 1).withIndex()
+            .firstNotNullOf { indexedChars -> indexedChars.index.takeIf { allUnique(indexedChars.value) } }
+            .let { it + groupSize }
 
     fun scenarioOne(textFile: String) = firstUnique(textFile, 4)
 
